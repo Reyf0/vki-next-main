12b931a12eeedc65afe6a9a4c028a66883f12477
shpak2207b <shpaktp@mer.ci.nsu.ru>
2025-12-19 17:44:57 +0700

    Postgres

--- src/db/AppDataSource.ts
+++ src/db/AppDataSource.ts
@@ -1,15 +1,25 @@
 import 'reflect-metadata';
-import { DataSource } from 'typeorm';
+import {DataSource, DataSourceOptions} from 'typeorm';
 import { Group } from './entity/Group.entity';
 import { Student } from './entity/Student.entity';
 import { User } from './entity/User.entity';
 
+const timeout = 30000;
+
 const AppDataSource = new DataSource({
-  type: 'sqlite',
-  database: process.env.DB ?? './db/vki-web.db',
-  entities: [Group, Student, User],
-  synchronize: true,
-  logging: false,
+    type: 'postgres',
+    url: process.env.DATABASE_URL!, // <-- важно
+    ssl: {
+        rejectUnauthorized: false,
+    },
+    extra: {
+        connectionTimeoutMillis: timeout,
+    },
+    synchronize: process.env.NODE_ENV !== 'production',
+    migrationsRun: process.env.NODE_ENV === 'production',
+    logging: false,
+    entities: [Student, Group, User],
 });
+
 
 export default AppDataSource;
